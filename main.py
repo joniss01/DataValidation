4805210fefa9bf10e4bb47b8ffe0fe5bd0b8d306
joniss01 <jonathan.nissen24@gmail.com>
2024-04-16 10:38:38 -0500

    set up functions

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ This data validator will read input.csv and validate valid data and send it to v
 invalid.csv.
 
 The data validator will check for:
-    -Invalid id that isn't an integer
-    -Invalid names that are not in a "last name, first name" format
-    -Invalid email that are not in proper email format or not .edu extension
-    -Invalid phone numbers that are not in a 111-222-3333 format
-    -Invalid date that are not in a MM/DD/YYYY format
-    -Invalid time that are not in a HH:MM military format
+    C = Invalid data element count
+    I = Invalid id that isn't an integer
+    N = Invalid names that are not in a "last name, first name" format
+    E = Invalid email that are not in proper email format or not .edu extension
+    P = Invalid phone numbers that are not in a 111-222-3333 format
+    D = Invalid date that are not in a MM/DD/YYYY format
+    T = Invalid time that are not in a HH:MM military format
+
+A report will be displayed to show how many records validated successfully along with an error report of each invalid
+data type from above.
 """
 
 __author__ = 'Jonathan Nissen'
@@ -19,10 +23,44 @@ __date__ = '4/16/2024'
 __status__ = 'Development'
 
 
-def print_hi(name):
-    print(f'Hi, {name}')
+def validate_id(id):
+    # regex [0-9]?
+    # if re.compile(regex, id):
+    #    return
+    pass
+
+
+def validate_name(name):
+    # last, first
+    pass
+
+
+def validate_email(email):
+    # need @ and .edu
+    pass
+
+
+def validate_phone(phone):
+    # 111-222-3333
+    pass
+
+
+def validate_date(date):
+    # MM/DD/YYYY
+    pass
+
+
+def validate_time(time):
+    # HH:MM military
+    pass
+
+
+def process_file():
+    # with open("input.csv") as my_file:
+    #     ?print?(my_file.read())
+    pass
 
 
 if __name__ == '__main__':
-    print_hi('Jonathan Nissen')
+    process_file()
 
